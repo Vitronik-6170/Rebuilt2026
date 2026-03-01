fe488039f1ca3fa2f9aad6473f5dcc7944b61125
admin <186482@upslp.edu.mx>
2026-03-01 17:37:21 -0600

    Modificaciones en Robot Container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,11 +23,15 @@ import frc.robot.subsystems.Pivot;
 import frc.robot.subsystems.Shooter;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 
@@ -72,18 +76,13 @@ public class RobotContainer {
     // Default drive command
     m_robotDrive.setDefaultCommand(
         new RunCommand(
-            () -> m_robotDrive.drive(
-                -MathUtil.applyDeadband(
-                    m_driverController.getLeftY(),
-                    OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(
-                    m_driverController.getLeftX(),
-                    OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(
-                    m_driverController.getRightX(),
-                    OIConstants.kDriveDeadband),
+                () -> m_robotDrive.drive(
+                -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
                 true),
             m_robotDrive));
+
 
     // Auto chooser con PathPlanner
     autoChooser = AutoBuilder.buildAutoChooser();
